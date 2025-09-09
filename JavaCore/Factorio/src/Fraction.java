43af20f3e9bdd18f53c2b223f3255d1b4c4adc9b
Kirill <krupeto302@gmail.com>
2025-09-10 00:40:17 +0300

    made edits to merge

--- JavaCore/Factorio/src/Fraction.java
+++ JavaCore/Factorio/src/Fraction.java
@@ -1,39 +1,40 @@
 import java.util.HashMap;
 import java.util.List;
 
-public class Fraction extends Thread{
-    private String name;
-    private DetailFactory factory;
-    private HashMap<Parts, Integer> myInventory = new HashMap<>();
+public class Fraction extends Thread {
 
-    public Fraction(String name, DetailFactory factory) {
-        this.name = name;
-        this.factory = factory;
-    }
+  private String name;
+  private DetailFactory factory;
+  private HashMap<Parts, Integer> myInventory = new HashMap<>();
 
-    public void run(){
-        for(int i=0; i < 100; i++){
-            List<Parts> newParts;
-            try {
-                newParts = factory.takeParts(5);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-            addToInventory(newParts);
-        }
-    }
+  public Fraction(String name, DetailFactory factory) {
+    this.name = name;
+    this.factory = factory;
+  }
 
-    private void addToInventory(List<Parts> newParts) {
-        for(Parts p : newParts){
-            myInventory.merge(p, 1, Integer::sum);
-        }
+  public void run() {
+    for (int i = 0; i < 100; i++) {
+      List<Parts> newParts;
+      try {
+        newParts = factory.takeParts(5);
+      } catch (InterruptedException e) {
+        throw new RuntimeException(e);
+      }
+      addToInventory(newParts);
     }
+  }
 
-    public int calculateArmy(){
-        int heads = myInventory.getOrDefault(Parts.Head,0);
-        int torso = myInventory.getOrDefault(Parts.torso, 0);
-        int hands = myInventory.getOrDefault(Parts.hand, 0)/2;
-        int feet = myInventory.getOrDefault(Parts.feet, 0)/2;
-        return Math.min(Math.min(heads, torso), Math.min(hands, feet));
+  private void addToInventory(List<Parts> newParts) {
+    for (Parts p : newParts) {
+      myInventory.merge(p, 1, Integer::sum);
     }
+  }
+
+  public int calculateArmy() {
+    int heads = myInventory.getOrDefault(Parts.HEAD, 0);
+    int torso = myInventory.getOrDefault(Parts.TORSO, 0);
+    int hands = myInventory.getOrDefault(Parts.HAND, 0) / 2;
+    int feet = myInventory.getOrDefault(Parts.FEET, 0) / 2;
+    return Math.min(Math.min(heads, torso), Math.min(hands, feet));
+  }
 }
